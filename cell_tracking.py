fd84b41184170f41a708b3f3c0d49641d91a80c1
Meta Novitia <mnovitia@uci.edu>
2018-11-27 16:21:57 -0800

    Added successful program for test case

---
+++ cell_tracking.py
@@ -0,0 +1,54 @@
+### Meta Novitia
+### Project 0  : Physics-based tracking of moving and growing cells in a colony
+### Start Date : November 27, 2018
+
+from imgpy import Img
+
+def formatImage(gifname,fname):
+    with Img(fp=gifname) as im:
+        im.crop(box=(im.width / 2, 0, im.width, im.height))
+        for frame in im.frames:
+            for x in range(im.width):
+                for y in range(im.height):
+                    if frame.getpixel((x,y))==48:
+                        frame.putpixel((x,y),32)
+                    if frame.getpixel((x,y))!=32 and frame.getpixel((x,y))!=60:
+                        frame.putpixel((x,y),6)                        
+        im.save(fp=fname)
+
+def start(fname, output):
+    file = open(output,"w")
+    with Img(fp=fname) as im:
+        f = im.frames
+        for index in range(len(f)):
+            frame = f[index]
+            ct = 0
+            for x in range(1,frame.width-1):
+                for y in range(1,frame.height-1):
+                    if frame.getpixel((x,y))==2 and (frame.getpixel((x+1,y))==1 or frame.getpixel((x-1,y))==1 or frame.getpixel((x,y+1))==1 or frame.getpixel((x,y-1))==1):
+                        # bfs
+                        area = 1
+                        queue = [(x,y)]
+                        while queue!=[]:
+                            a,b = queue.pop(0)
+                            for i in range(-1,2):
+                                for j in range(-1,2):
+                                    if (i==0 or j==0) and frame.getpixel((a+i,b+j))!=0:
+                                        if frame.getpixel((a+i,b+j))==2:
+                                            area+=1
+                                            queue.append((a+i,b+j))                                          
+                                        frame.putpixel((a+i,b+j),0)
+                        if area > 40:ct+=1
+            file.write(str(ct)+'\n')
+    file.close()
+
+
+gifname = '/Users/meta2/Desktop/bacteria-animation.gif' # original gif
+fname = '/Users/meta2/Desktop/crop.gif'
+output = 'output.txt'
+
+formatImage(gifname,fname)   
+start(fname, output)         
+
+# dfs on cells until meet source point, check cell area if bigger than 40 pixels
+    # black : 0, red : 1, gray : 2
